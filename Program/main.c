6b58b608a553b8a6572bd2e44dd7e3f195a6a156
Xiaoxiao1990 <461146760@qq.com>
2016-06-30 14:46:44 +0800

    Update main.c

--- Program/main.c
+++ Program/main.c
@@ -1,34 +1,20 @@
 /* MAIN.C file
- * 
  * Copyright (c) 2002-2005 STMicroelectronics
  */
 #include "includes.h"
 #define SCLK(c) 		(c)?(CLK_CCOR = 0B00001101):(CLK_CCOR = 0x00)//2M
 #define POWER				PD_ODR_0
 #define RST					PD_ODR_4
-main()
+void main(void)
 {
 	uchar status;
 	_configSet();
 	POWER = 1;
-//	_timer2_Initial();
 	_timerBaseInitial();
-//	_IOInitial();
-//	_LCD_Init();
 	_uartInit();
-	//_EEPROM_Initial();
-	/*Watch Dog*/
-	//IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-	//IWDG_SetPrescaler(IWDG_Prescaler_64);
-	//IWDG_SetReload(0xff);
-	//IWDG_ReloadCounter();
-	//IWDG_Enable();
-	//IWDG_WriteAccessCmd(IWDG_WriteAccess_Disable);
 	while(1)
 	{
-		//_displayAndKeyscan();
 		_timerBase();
-		//IWDG_ReloadCounter();//clear counter.
 		switch(status)
 		{
 			case 0:
@@ -70,17 +56,9 @@ main()
 			}break;
 			default:;
 		}
-		if(isTimeProcess)//about 4ms
+		if(isTimeProcess)//about 500us
 		{
 			isTimeProcess = No;
-			//_LCD_BackLight(1);
-		//	_LCD_Init();
-			//_output();
-			//_timerWheel();
-			//_displayDecode();
-			//_keysProcess();
-			//_LCD_Update();
 		}
-		//_messageProcess();
 	}
 }
