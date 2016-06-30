75718306e37df98006cf52e3d6061827ed229644
Xiaoxiao1990 <461146760@qq.com>
2016-06-30 17:37:23 +0800

    Update main.c

--- Program/main.c
+++ Program/main.c
@@ -1,33 +1,46 @@
 /* MAIN.C file
+ * 
  * Copyright (c) 2002-2005 STMicroelectronics
  */
 #include "includes.h"
 #define SCLK(c) 		(c)?(CLK_CCOR = 0B00001101):(CLK_CCOR = 0x00)//2M
 #define POWER				PD_ODR_0
 #define RST					PD_ODR_4
-void main(void)
+main()
 {
 	uchar status;
 	_configSet();
-	POWER = 1;
+//	_timer2_Initial();
 	_timerBaseInitial();
+//	_IOInitial();
+//	_LCD_Init();
 	_uartInit();
+	//_EEPROM_Initial();
+	/*Watch Dog*/
+	//IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+	//IWDG_SetPrescaler(IWDG_Prescaler_64);
+	//IWDG_SetReload(0xff);
+	//IWDG_ReloadCounter();
+	//IWDG_Enable();
+	//IWDG_WriteAccessCmd(IWDG_WriteAccess_Disable);
 	while(1)
 	{
+		//_displayAndKeyscan();
 		_timerBase();
+		//IWDG_ReloadCounter();//clear counter.
 		switch(status)
 		{
 			case 0:
 			{
-				POWER = 1;
+				POWER = 0;
 				RST = 0;
 				SCLK(0);
 				isRunTimer = 1;
 				if(TimerCNT >= 10000)//5s
 				{
 					isRunTimer = 0;
-					POWER = 0;
-					SCLK(1);
+					TimerCNT = 0;
+					POWER = 1;
 					status++;
 				}
 			}break;
@@ -35,8 +48,9 @@ void main(void)
 			case 2://wait for Tb
 			{
 				isRunTimer = 1;
-				if(TimerCNT > 2)
-				{//Tb >= 200us
+				if(TimerCNT > 40)
+				{//Tc >= 20000us
+					TimerCNT = 0;
 					isRunTimer = 0;
 					RST = 1;
 					status++;
@@ -44,21 +58,31 @@ void main(void)
 			}break;
 			case 3:
 			{
-				//Read ATR
+				//Wait for ATR
+				isRunTimer = 1;
+				if(TimerCNT > 1)
+				{//Time out
+					TimerCNT = 0;
+					isRunTimer = 0;
+					status++;
+				}
+			}break;
+			case 4:
+			{
 				isRunTimer = 1;
 				if(TimerCNT >= 20000)
 				{//Time out
 					isRunTimer = 0;
+					TimerCNT = 0;
 					RxPoint = 0;
-					//status = 0;
-					//SCLK(0);
+					status++;
+					SCLK(0);
 				}
 			}break;
+			case 5:
+			status = 0;//break point for debug
+			break;
 			default:;
-		}
-		if(isTimeProcess)//about 500us
-		{
-			isTimeProcess = No;
 		}
 	}
 }
